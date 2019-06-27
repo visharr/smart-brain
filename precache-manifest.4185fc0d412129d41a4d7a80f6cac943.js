f231ea2976f3a8be1d6d5c6984e1df4f8f376b2a
Vashar <vishal.pilani@live.com>
2019-06-27 14:22:02 +0530

    Updates

---
+++ precache-manifest.4185fc0d412129d41a4d7a80f6cac943.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8d7b65f4947cbe11cb7a2e7f33a87a",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "d1ebd76a6ed8790ec0b5",
+    "url": "/smartbrain/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "db40bac81add5140dfae",
+    "url": "/smartbrain/static/css/main.65edcfb4.chunk.css"
+  },
+  {
+    "revision": "d1ebd76a6ed8790ec0b5",
+    "url": "/smartbrain/static/js/2.d533a9b1.chunk.js"
+  },
+  {
+    "revision": "db40bac81add5140dfae",
+    "url": "/smartbrain/static/js/main.f6c9e3e2.chunk.js"
+  },
+  {
+    "revision": "c811ced7da05cfde01c3",
+    "url": "/smartbrain/static/js/runtime~main.54e658c3.js"
+  }
+]);
